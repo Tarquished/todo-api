96f095ebfb46c0719a9b341bb4d3981f09e1dd13
Tarquished <jason.samuel001@binus.ac.id>
2026-05-05 11:08:45 +0700

    feat: add JWT Auth

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module todo-api
 
-go 1.22
+go 1.25.0
 
 require (
 	gorm.io/driver/postgres v1.6.0
@@ -8,13 +8,14 @@ require (
 )
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/crypto v0.50.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 	"strconv"
 
+	"context"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -39,6 +45,161 @@ type getTodo struct {
 
 type ResponError struct {
 	Error string `json:"error"`
+}
+
+type User struct {
+	gorm.Model
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type InputAuth struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func handlerRegister(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		sendError(w, "method harus POST", 405)
+		return
+	}
+
+	var input InputAuth
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		sendError(w, "format JSON tidak valid", 400)
+		return
+	}
+
+	if input.Username == "" {
+		sendError(w, "mohon isi username", 400)
+		return
+	}
+	if input.Password == "" {
+		sendError(w, "mohon isi password", 400)
+		return
+	}
+
+	var user User
+
+	results := db.Where("username = ?", input.Username).First(&user)
+	if results.Error == nil {
+		sendError(w, "username sudah ada", 400)
+		return
+	}
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), 14)
+	if err != nil {
+		sendError(w, "error saat hashing password", 400)
+		return
+	}
+	db.Create(&User{
+		Username: input.Username,
+		Password: string(hashPassword),
+	})
+	succesRespon := map[string]any{
+		"pesan": "Berhasil menambahkan username ke database",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(succesRespon)
+}
+
+func handlerLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		sendError(w, "method harus POST", 405)
+		return
+	}
+
+	var input InputAuth
+	err := json.NewDecoder(r.Body).Decode(&input)
+
+	if err != nil {
+		sendError(w, "format JSON salah", 400)
+		return
+	}
+	if input.Username == "" {
+		sendError(w, "mohon isi username", 400)
+		return
+	}
+	if input.Password == "" {
+		sendError(w, "mohon isi password", 400)
+		return
+	}
+
+	var user User
+
+	results := db.Where("username = ?", input.Username).First(&user)
+	if results.Error != nil {
+		sendError(w, "username belum ada", 400)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
+	if err != nil {
+		sendError(w, "password salah", 400)
+		return
+	}
+
+	claims := jwt.MapClaims{
+		"user_id":  user.ID,
+		"username": user.Username,
+		"exp":      time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	secretkey := os.Getenv("JWT_SECRET")
+	if secretkey == "" {
+		secretkey = "test1625jason34"
+	}
+	tokenString, err := token.SignedString([]byte(secretkey))
+
+	if err != nil {
+		sendError(w, "gagal generate token", 400)
+		return
+	}
+	succesRespon := map[string]any{
+		"pesan": "Berhasil login!",
+		"token": tokenString,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(succesRespon)
+}
+
+func algoritma(t *jwt.Token) (interface{}, error) {
+	secretkey := os.Getenv("JWT_SECRET")
+	if secretkey == "" {
+		secretkey = "test1625jason34"
+	}
+	return []byte(secretkey), nil
+}
+
+func verifyToken(r *http.Request) (*jwt.MapClaims, error) {
+	token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+	claims := &jwt.MapClaims{}
+
+	responToken, err := jwt.ParseWithClaims(token, claims, algoritma)
+
+	if err != nil {
+		return nil, err
+	}
+	if !responToken.Valid {
+		return nil, fmt.Errorf("token tidak valid")
+	}
+	return claims, nil
+}
+
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		claims, err := verifyToken(r)
+		if err != nil {
+			sendError(w, "tidak valid", 401)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "claims", claims)
+		next(w, r.WithContext(ctx))
+	}
 }
 
 func sendError(w http.ResponseWriter, message string, statusCode int) {
@@ -231,12 +392,14 @@ func main() {
 	}
 
 	db.AutoMigrate(&Todo{})
-
-	http.HandleFunc("/tambah-todo", handlerTodoSingle)
-	http.HandleFunc("/tambah-todo-batch", handlerTodoBatch)
-	http.HandleFunc("/todos", handlerTodos)
-	http.HandleFunc("/hapus-todo", handlerHapusTodo)
-	http.HandleFunc("/update-todo", handlerUpdateTodo)
+	db.AutoMigrate(&User{})
+	http.HandleFunc("/register", handlerRegister)
+	http.HandleFunc("/login", handlerLogin)
+	http.HandleFunc("/tambah-todo", authMiddleware(handlerTodoSingle))
+	http.HandleFunc("/tambah-todo-batch", authMiddleware(handlerTodoBatch))
+	http.HandleFunc("/todos", authMiddleware(handlerTodos))
+	http.HandleFunc("/hapus-todo", authMiddleware(handlerHapusTodo))
+	http.HandleFunc("/update-todo", authMiddleware(handlerUpdateTodo))
 
 	port := os.Getenv("PORT")
 	if port == "" {
